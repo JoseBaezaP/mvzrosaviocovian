484464d3d09096e94cf67adbf6b7f9bf56fd46b7
Jose Baeza <jose.baeza@divelement.io>
2025-05-30 12:58:16 -0600

    fix: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://josebaezap.github.io",
+  base: "/mvzrosaviocovian.github.io/",
 
   vite: {
     plugins: [tailwindcss()],
